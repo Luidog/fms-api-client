1e6e32b254c5b3a8789a06b28fa7ddc712f4dcbb
Lui de la Parra <lui@mutesymphony.com>
2018-05-19 23:06:49 -0700

    Expands Example
    This commit expands the example documentation with comments.
    
    Signed-off-by: Lui de la Parra <lui@mutesymphony.com>

--- example.js
+++ example.js
@@ -11,12 +11,11 @@ environment.config({ path: './tests/.env' });
 varium(process.env, './tests/env.manifest');
 
 /**
- * Connect must be called before the filemaker class is instiantiated. This connect uses Marpat. Marpat is a fork of
- * Camo. much love to https://github.com/scottwrobinson for his creation and maintenance of Camo.
- * My fork of Camo - Marpat is designed to allow the use of multiple datastores with the focus on encrypted storage.
- * @param  {url} url a url string representing a datastore.
- * @param  {options} options an object representing datastore options. See Marpat for more info.
- * @return {Promise}           A database.
+ * Connect must be called before the filemaker class is instiantiated. This
+ * connect uses Marpat. Marpat is a fork of Camo. much love to
+ * https://github.com/scottwrobinson for his creation and maintenance of Camo.
+ * My fork of Camo - Marpat is designed to allow the use of multiple datastores
+ * with the focus on encrypted storage.
  */
 
 connect('nedb://memory').then(db => {
@@ -33,10 +32,16 @@ connect('nedb://memory').then(db => {
   });
 
   /**
-   * A client can be used directly after saving it. It is also stored on the datastore
-   * so that it can be reused later.
+   * A client can be used directly after saving it. It is also stored on the
+   * datastore so that it can be reused later.
    */
   client.save().then(client =>
+    /**
+     * Using the client you can create filemaker records. To create a record
+     * specify the layout to use and the data to insert on creation. The client
+     * will automatically convert numbers, arrays, and objects into strings so
+     * they can be inserted into a filemaker field.
+     */
     client
       .create('Heroes', {
         name: 'George Lucas',
@@ -44,13 +49,16 @@ connect('nedb://memory').then(db => {
         array: ['1'],
         object: { driods: true }
       })
-      .then(record => {
-        console.log(record);
-        console.log('Some guy thought of a movie....'.yellow.underline, record);
-      })
+      .then(record =>
+        console.log('Some guy thought of a movie....'.yellow.underline, record)
+      )
       .catch(error => console.log('That is no moon....'.red, error))
   );
-
+  /**
+   * Most methods on the client are promises. The only exceptions to this are
+   * the utility methods of fieldData(), and recordId(). You can chain together
+   * multiple methods such as record creation.
+   */
   client
     .save()
     .then(client => {
@@ -64,6 +72,12 @@ connect('nedb://memory').then(db => {
       });
     })
     .then(client => {
+      /**
+       * You can use the client to list filemaker records. The List method
+       * accepts a layout and parameter variable. The client will automatically
+       * santize the limit, offset, and sort keys to correspond with the Data
+       * API's requirements.
+       */
       client
         .list('Heroes', { limit: 5 })
         .then(response => client.fieldData(response.data))
@@ -75,7 +89,9 @@ connect('nedb://memory').then(db => {
           )
         )
         .catch(error => console.log('That is no moon....'.red, error));
-
+      /**
+       * You can also use the client to set FileMaker Globals for the session.
+       */
       client
         .globals({ 'Globals::ship': 'Millenium Falcon' })
         .then(response =>
@@ -91,6 +107,12 @@ connect('nedb://memory').then(db => {
       return client;
     })
     .then(client => {
+      /**
+       * The client's find method  will accept either a single object as find
+       * parameters or an array. The find method will also santize the limit,
+       * sort, and offset parameters to conform with the Data API's
+       * requirements.
+       */
       client
         .find('Heroes', [{ name: 'Anakin Skywalker' }], { limit: 1 })
         .then(response => client.recordId(response.data))
