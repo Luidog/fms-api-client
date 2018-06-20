c5902378fe2e1fcb24ef70765760a417f78be9b3
Lui de la Parra <Lui@mutesymphony.com>
2018-06-19 20:03:33 -0700

    Revises Method Documentation
    This commit revises the documentation of utility methods.
    
    Signed-off-by: Lui de la Parra <Lui@mutesymphony.com>

--- src/utilities.service.js
+++ src/utilities.service.js
@@ -3,25 +3,24 @@
 const _ = require('lodash');
 
 /**
-  /**
-   * @method _toArray
-   * @private
-   * @memberof Client
-   * @description _toArray is a helper method that converts an object into an array. This is used 
-   * @param  {Object|Array} data the raw data returned from a filemaker. This can be an array or an object.
-   * @return {Object}      a json object containing stringified data.
-   */
+ * @method toArray
+ * @description The toArray method converts an object into an array. This method uses the object prototype method
+ * isArray to check if the incoming data is an array. If the incoming data is not an array this method will
+ * return the data in an array
+ * @param  {Object|Array} data An array or object containing query information. This can be an array or an object.
+ * @return {Object}      An array containing the data passed to the method.
+ */
+
 const toArray = data => (Array.isArray(data) ? data : [data]);
 
 /**
- * @method _namespace
- * @private
- * @memberof Client
- * @description This method filters the FileMaker DAPI response by testing if a script was triggered with
- * the request, then either selecting the response, script error, and script result from the response or
- * selecting just the response.
- * @return {Object}      a json object containing the selected data from the Data API Response.
+ * @method namespace
+ * @description The namespace method maps through an incoming data object's keys and replaces the properties
+ * of limit, offset, and sort with their _ counterparts.
+ * @param  {Object} data An object used in a DAPI query.
+ * @return {Object}      A modified object containing modified keys to match expected properties
  */
+
 const namespace = data =>
   _.mapKeys(
     data,
@@ -30,12 +29,13 @@ const namespace = data =>
   );
 
 /**
- * @method _isJson
- * @private
- * @memberof Client
- * @description This is a helper method for the _filterResponse method.
- * @return {Boolean}      a boolean result if the data passed to it is json
+ * @method isJson
+ * @description The isJson method uses the a try / catch to parse incoming data safely as json.
+ * This method will return tru if it is able to cast the incoming data as json.
+ * @param  {Any} data The data to be evaluated as json.
+ * @return {Boolean}      A boolean result depending on if the data passed to it is valid JSON
  */
+
 const isJson = data => {
   try {
     JSON.parse(data);
@@ -46,13 +46,12 @@ const isJson = data => {
 };
 
 /**
- * @method _stringify
- * @private
- * @memberof Client
- * @description _stringify is a helper method that converts numbers and objects / arrays to strings.
- * @param  {Object|Array} The data being used to create or update a record.
+ * @method stringify
+ * @description stringify is a helper method that converts numbers, objects, and arrays to strings.
+ * @param  {Object|Array} data The data being used to create or update a record.
  * @return {Object}      a json object containing stringified data.
  */
+
 const stringify = data =>
   _.mapValues(
     data,
@@ -63,13 +62,12 @@ const stringify = data =>
   );
 
 /**
- * @method _filterResponse
- * @private
- * @memberof Client
- * @description This method filters the FileMaker DAPI response by testing if a script was triggered with
- * the request, then either selecting the response, script error, and script result from the response or
- * selecting just the response.
- * @return {Object}      a json object containing the selected data from the Data API Response.
+ * @method filterResponse
+ * @description This method filters the FileMaker DAPI response by testing if a script was triggered
+ * with the request, then either selecting the response, script error, and script result from the
+ * response or selecting just the response.
+ * @param  {Object} data The response recieved from the FileMaker DAPI.
+ * @return {Object}      A json object containing the selected data from the Data API Response.
  */
 const filterResponse = data =>
   _.mapValues(
@@ -77,18 +75,17 @@ const filterResponse = data =>
     value => (isJson(value) ? JSON.parse(value) : value)
   );
 
-const map = (data, iteratee) => _.map(data, iteratee);
-
 /**
- * @method _sanitizeParameters
- * @memberof Client
- * @private
- * @param {Object} layout the parameters to use when filtering safe parameters and stringifying values
- * @param {Array} safeParameters String values to allow to be sent to filemaker.
- * @description stringifys all values for an object. This is used to ensure that find requests and list requests
- * can use either strings or numbers when setting options.
- * @return {Object} returns an object with all safe keys and values mapped to strings.
+ * @method sanitizeParameters
+ * @description The santizeParameters method filters unsafe parameters from its return object based
+ * on the safeOarameters array that is passed to it. This method is currently used in the client.create
+ * method to seperate the merge option from the paramaters that are safe to send to FileMaker.
+ * @param {Array} parameters An array values being passed to the FileMaker DAPI.
+ * @param {Array} safeParameters An array values allowed to be sent to filemaker.
+ * @return {Object|Array} returns an object or array of objects with only allowed keys
+ * and values mapped to strings.
  */
+
 const sanitizeParameters = (parameters, safeParameters) =>
   safeParameters
     ? _.mapValues(
@@ -99,6 +96,16 @@ const sanitizeParameters = (parameters, safeParameters) =>
         parameters,
         value => (_.isNumber(value) ? value.toString() : value)
       );
+
+/**
+ * @method map
+ * @description The map method abstracts the lodash map method to reduce the number of imports required
+ * for each model. This method accepts an array and a method to use when mapping the array of values.
+ * @param  {Array} data The data to use when invoking the method
+ * @param  {Function} iteratee The function to invoke on each item in the array
+ * @return {Array}          The mutated array of values after each value is passed to the iteratee method.
+ */
+const map = (data, iteratee) => _.map(data, iteratee);
 
 module.exports = {
   toArray,
