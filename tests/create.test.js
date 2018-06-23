ddb1e7c00263d661136a8bc2720a50ba56f30294
Lui de la Parra <lui@mutesymphony.com>
2018-06-23 14:17:57 -0700

    Adds Create Tests
    This commit adds tests for converting a scripts array to script parameters.
    
    Signed-off-by: Lui de la Parra <lui@mutesymphony.com>

--- tests/create.test.js
+++ tests/create.test.js
@@ -126,6 +126,71 @@ describe('Create Capabilities', () => {
       );
   });
 
+  it('should allow you to specify scripts as an array', () => {
+    return expect(
+      filemaker.create(
+        process.env.LAYOUT,
+        {
+          name: 'Han Solo',
+          array: ['ben'],
+          object: { 'co-pilot': 'chewbacca' },
+          height: 52
+        },
+        {
+          scripts: [
+            { name: 'FMS Triggered Script', param: 'data' },
+            {
+              name: 'FMS Triggered Script',
+              phase: 'prerequest',
+              param: { data: 2 }
+            }
+          ]
+        }
+      )
+    )
+      .to.eventually.be.a('object')
+      .that.has.all.keys(
+        'recordId',
+        'modId',
+        'scriptResult',
+        'scriptError.prerequest',
+        'scriptResult.prerequest',
+        'scriptError'
+      );
+  });
+
+  it('should allow you to specify scripts as an array with a merge response', () => {
+    return expect(
+      filemaker.create(
+        process.env.LAYOUT,
+        {
+          name: 'Han Solo',
+          array: ['ben'],
+          object: { 'co-pilot': 'chewbacca' },
+          height: 52
+        },
+        {
+          script: 'FMS Triggered Script',
+          merge: true,
+          scripts: [{ name: 'FMS Triggered Script', phase: 'prerequest' }]
+        }
+      )
+    )
+      .to.eventually.be.a('object')
+      .that.has.all.keys(
+        'recordId',
+        'name',
+        'array',
+        'object',
+        'height',
+        'modId',
+        'scriptResult',
+        'scriptError.prerequest',
+        'scriptResult.prerequest',
+        'scriptError'
+      );
+  });
+
   it('should sanitize parameters when creating a new record', () => {
     return expect(
       filemaker.create(
@@ -149,6 +214,39 @@ describe('Create Capabilities', () => {
         'modId',
         'scriptResult',
         'scriptError'
+      );
+  });
+
+  it('should accept both the default script parameters and a scripts array', () => {
+    return expect(
+      filemaker.create(
+        process.env.LAYOUT,
+        {
+          name: 'Han Solo',
+          array: ['ben'],
+          object: { 'co-pilot': 'chewbacca' },
+          height: 52
+        },
+        {
+          script: 'FMS Triggered Script',
+          'script.param': 1,
+          merge: true,
+          scripts: [{ name: 'FMS Triggered Script', phase: 'prerequest',param:2 }]
+        }
+      )
+    )
+      .to.eventually.be.a('object')
+      .that.has.all.keys(
+        'recordId',
+        'name',
+        'array',
+        'object',
+        'height',
+        'modId',
+        'scriptResult',
+        'scriptError',
+        'scriptError.prerequest',
+        'scriptResult.prerequest'
       );
   });
 });
