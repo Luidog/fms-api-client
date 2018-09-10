58e9ffcc58a60fbdb7b9a0f67593b802f40f5dd1
Lui de la Parra <lui@mutesymphony.com>
2018-09-09 23:06:14 -0700

    Implements Axios Interceptors
    This commit implements axios request and response interceptors to better handle DAPI errors and request handling.
    
    Signed-off-by: Lui de la Parra <lui@mutesymphony.com>

--- src/request.service.js
+++ src/request.service.js
@@ -2,21 +2,47 @@
 
 const axios = require('axios');
 const FormData = require('form-data');
-const _ = require('lodash');
+const { omit } = require('./utilities.service');
 
 const request = axios.create();
 
-const sanitize = request => _.omit(request, ['params.request', 'data.request']);
+/**
+ * @function interceptRequest
+ * @description handles request data before it is sent to the resource. This method
+ * will eventually be used to cancel the request and return the configuration body.
+ * This function will re the request params and data request properties.
+ * @param  {Object} config The axios request configuration
+ * @return {Promise}      the request configuration object
+ */
 
-const intercept = config =>
-  config.params
-    ? config.params.request
-      ? Promise.resolve(sanitize(config))
-      : sanitize(config)
-    : sanitize(config);
+const interceptRequest = config =>
+  omit(config, ['params.request', 'data.request']);
 
-const handleError = error => Promise.reject(error);
+/**
+ * @function handleResponseError
+ * @description handles a 502 error for the client model. If the request
+ * generates a 502 status a message and code is generated for the rejection.
+ * @param  {Object} error The error recieved from the requested resource.
+ * @return {Promise}      A promise rejection containing a code and a message
+ */
 
-request.interceptors.request.use(intercept, handleError);
+const handleResponseError = error =>
+  error.response.status === 502
+    ? Promise.reject({
+        message: 'The Data API is currently unavailable',
+        code: '1630'
+      })
+    : Promise.reject(error.response.data.messages[0]);
+
+request.interceptors.request.use(interceptRequest, error =>
+  Promise.reject(error)
+);
+
+request.interceptors.response.use(response => response, handleResponseError);
+
+/**
+ * @module request
+ * @module FormData
+ */
 
 module.exports = { request, FormData };
