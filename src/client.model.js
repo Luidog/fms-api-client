bf85407b38b9872c73c9f7ebf691f39ab79790b4
Lui de la Parra <lui@mutesymphony.com>
2018-09-24 23:02:01 -0700

    Revises Upload Method
    This commit revises the upload method by modifying the promise chain to allow the correct recordId to be returned in the response.
    
    Signed-off-by: Lui de la Parra <lui@mutesymphony.com>

--- src/client.model.js
+++ src/client.model.js
@@ -686,33 +686,33 @@ class Client extends Document {
       form.append('upload', stream);
 
       resolveRecordId()
-        .then(recordId =>
-          this.authenticate().then(token =>
-            request.post(
-              this._uploadURL(
-                layout,
-                recordId,
-                containerFieldName,
-                fieldRepetition
-              ),
-              form,
-              {
-                headers: {
-                  ...form.getHeaders(),
-                  Authorization: `Bearer ${token}`
+        .then(resolvedId =>
+          this.authenticate()
+            .then(token =>
+              request.post(
+                this._uploadURL(
+                  layout,
+                  resolvedId,
+                  containerFieldName,
+                  fieldRepetition
+                ),
+                form,
+                {
+                  headers: {
+                    ...form.getHeaders(),
+                    Authorization: `Bearer ${token}`
+                  }
                 }
-              }
+              )
             )
-          )
+            .then(response => response.data)
+            .then(body => this.data.outgoing(body))
+            .then(body => this.connection.extend(body))
+            .then(body => this._saveState(body))
+            .then(body => filterResponse(body))
+            .then(response => Object.assign(response, { recordId: resolvedId }))
         )
-        .then(response => response.data)
-        .then(body => this.data.outgoing(body))
-        .then(body => this.connection.extend(body))
-        .then(body => this._saveState(body))
-        .then(body => filterResponse(body))
-        .then(response =>
-          resolve(Object.assign(response, { recordId: recordId }))
-        )
+        .then(response => resolve(response))
         .catch(error => reject(this._checkToken(error)));
     });
   }
