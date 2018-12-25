1e72d25e70abab395280c2fab804236ced92f323
Lui de la Parra <Lui@mutesymphony.com>
2018-12-24 20:18:06 -0800

    Updates Example Services
    This commit updates the example services to replace ids. This should result in less volatile examples.
    
    Signed-off-by: Lui de la Parra <Lui@mutesymphony.com>

--- examples/services/logger.service.js
+++ examples/services/logger.service.js
@@ -7,19 +7,27 @@ const deepMap = require('deep-map');
 const toFile = file => path.resolve(__dirname, '../results', `${file}.json`);
 
 const filter = values =>
-  deepMap(
-    values,
-    value =>
-      typeof value === 'string' && value.includes(process.env.SERVER)
-        ? value.replace(process.env.SERVER, 'https://some-server.com')
-        : value
-  );
+  deepMap(values, (value, key) => {
+    if (typeof value === 'string' && value.includes(process.env.SERVER)) {
+      return value.replace(process.env.SERVER, 'https://some-server.com');
+    } else if (key === 'recordId') {
+      return '1138';
+    } else if (key === 'modId') {
+      return '327';
+    } else if (key === 'modificationTimestamp' || key === 'creationTimestamp') {
+      return '05/25/1977 6:00:00';
+    } else if (key === 'id' || key === 'creationTimestamp') {
+      return 'r2d2-c3po-L3-37-bb-8';
+    } else {
+      return value;
+    }
+  });
 
 const log = (file, result) => {
   if (process.env.RESULTS) {
     fse.outputJson(toFile(file), filter(result), { spaces: 2 });
   }
-  console.log(result)
+  console.log(result);
   return result;
 };
 
